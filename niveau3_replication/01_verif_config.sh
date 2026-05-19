8f398b1b42644943062e77d36994a7ab318b1ffb
Mattis Tajan <mattis.tajan@ecoles-epsi.net>
2026-05-19 14:28:10 +0200

    Correction du script de verifictation de config, rajout du nom de la BDD

--- niveau3_replication/01_verif_config.sh
+++ niveau3_replication/01_verif_config.sh
@@ -1,14 +1,15 @@
-#!/bin/bash 
-# niveau3_replication/01_verif_config.sh 
- 
-echo '=== [NIVEAU 3] Verification de la configuration ===' 
- 
-WAL_LEVEL=$(docker exec pg_montpellier psql -U admin -t -c 'SHOW wal_level;' | tr -d 
-' ') 
-echo "WAL level source : $WAL_LEVEL" 
- 
-if [ "$WAL_LEVEL" != 'logical' ]; then 
-    echo 'ERREUR : wal_level doit etre logical pour la replication'     exit 1 fi 
- 
-echo 'OK : Configuration WAL correcte' 
-docker exec pg_montpellier psql -U admin -c 'SELECT version();' 
+#!/bin/bash
+# niveau3_replication/01_verif_config.sh
+
+echo '=== [NIVEAU 3] Verification de la configuration ==='
+
+WAL_LEVEL=$(docker exec pg_montpellier psql -U admin -d techcorp_db -t -c 'SHOW wal_level;' | tr -d ' ')
+echo "WAL level source : $WAL_LEVEL"
+
+if [ "$WAL_LEVEL" != 'logical' ]; then
+    echo 'ERREUR : wal_level doit etre logical pour la replication'
+    exit 1
+fi
+
+echo 'OK : Configuration WAL correcte'
+docker exec pg_montpellier psql -U admin -d techcorp_db -c 'SELECT version();'
