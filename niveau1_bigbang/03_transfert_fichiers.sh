59801334e9e2766f3479197b4a26af7ff76fd80a
Mattis Tajan <mattis.tajan@ecoles-epsi.net>
2026-05-19 16:41:53 +0200

    Modification du script de tranfert de fichiers avec rsync et ajout d'une comparaison checksums MD5

--- niveau1_bigbang/03_transfert_fichiers.sh
+++ niveau1_bigbang/03_transfert_fichiers.sh
@@ -1,28 +1,39 @@
 #!/bin/bash
-# niveau1_bigbang/03_transfert_fichiers.sh
+
 echo '=== [NIVEAU 1] Transfert des fichiers ==='
+
 SOURCE='./data_montpellier/supports/'
 CIBLE='./data_toulouse/supports/'
-# Options rsync :
-# -a : mode archive (preserve permissions, dates, liens symboliques)
-# -v : verbose (affiche les fichiers transferes)
-# -h : tailles lisibles (Ko, Mo, Go)
-# --progress : affiche la progression fichier par fichier
-# --checksum : verifie l'integrite par checksum (plus sur)
+
 rsync -avh --progress --checksum $SOURCE $CIBLE
+
 echo ''
 echo '=== Verification du transfert ==='
+
 NB_SOURCE=$(find $SOURCE -type f | wc -l)
 NB_CIBLE=$(find $CIBLE -type f | wc -l)
+
 echo "Fichiers source : $NB_SOURCE"
-echo "Fichiers cible : $NB_CIBLE"
+echo "Fichiers cible  : $NB_CIBLE"
+
 if [ $NB_SOURCE -eq $NB_CIBLE ]; then
-echo 'OK : meme nombre de fichiers'
+  echo 'OK : meme nombre de fichiers'
 else
-echo 'ERREUR : nombre de fichiers different !'
-exit 1
+  echo 'ERREUR : nombre de fichiers different !'
+  exit 1
 fi
-# TODO : Ajouter une comparaison des checksums MD5
-# Indice : md5sum data_montpellier/supports/* > checksums_source.txt
-# md5sum data_toulouse/supports/* > checksums_cible.txt
-# diff checksums_source.txt checksums_cible.txt
+
+echo ''
+echo '=== Verification des checksums MD5 ==='
+md5sum data_montpellier/supports/* > /tmp/checksums_source.txt
+md5sum data_toulouse/supports/*    > /tmp/checksums_cible.txt
+
+awk '{print $1}' /tmp/checksums_source.txt | sort > /tmp/hash_source.txt
+awk '{print $1}' /tmp/checksums_cible.txt  | sort > /tmp/hash_cible.txt
+
+if diff /tmp/hash_source.txt /tmp/hash_cible.txt > /dev/null; then
+  echo 'OK : Checksums identiques – fichiers integres'
+else
+  echo 'ERREUR : Checksums differents – fichiers corrompus !'
+  exit 1
+fi
