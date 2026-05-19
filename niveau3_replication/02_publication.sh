96d5ed0dcde29d81c6ba27696ecfb87996f8794b
Mattis Tajan <mattis.tajan@ecoles-epsi.net>
2026-05-19 14:31:55 +0200

    Ajout taches TODO script de publication, et correction indentation

--- niveau3_replication/02_publication.sh
+++ niveau3_replication/02_publication.sh
@@ -1,18 +1,29 @@
 #!/bin/bash 
 # niveau3_replication/02_publication.sh 
  
-echo '=== [NIVEAU 3] Creation de la PUBLICATION sur la source ===' 
- 
-# Creer une publication pour toutes les tables 
-docker exec pg_montpellier psql -U admin -d techcorp_db -c \   "CREATE PUBLICATION pub_techcorp FOR ALL TABLES;" 
- 
-# Verifier que la publication est creee 
-docker exec pg_montpellier psql -U admin -d techcorp_db -c \   "SELECT pubname, puballtables FROM pg_publication;" 
+echo '=== [NIVEAU 3] Creation de la PUBLICATION sur la source ==='
+
+# Creer une publication pour toutes les tables
+docker exec pg_montpellier psql -U admin -d techcorp_db -c \
+  "CREATE PUBLICATION pub_techcorp FOR ALL TABLES;"
+
+# Verifier que la publication est creee
+docker exec pg_montpellier psql -U admin -d techcorp_db -c \
+  "SELECT pubname, puballtables FROM pg_publication;"
  
 echo 'OK : Publication creee sur la source' 
  
-# Afficher les tables publiees 
-docker exec pg_montpellier psql -U admin -d techcorp_db -c \   "SELECT * FROM pg_publication_tables;" 
- 
-# TODO : Creer une publication partielle (seulement certaines tables) 
-# Indice : CREATE PUBLICATION pub_partielle FOR TABLE utilisateurs, formations; 
+# Afficher les tables publiees
+docker exec pg_montpellier psql -U admin -d techcorp_db -c \
+  "SELECT * FROM pg_publication_tables;"
+
+#TODO
+# Creer une publication partielle (seulement certaines tables)
+docker exec pg_montpellier psql -U admin -d techcorp_db -c \
+  "CREATE PUBLICATION pub_partielle FOR TABLE utilisateurs, formations;"
+
+# Verifier la publication partielle
+docker exec pg_montpellier psql -U admin -d techcorp_db -c \
+  "SELECT pubname, puballtables FROM pg_publication;"
+
+echo 'OK : Publication partielle creee (utilisateurs + formations uniquement)'
