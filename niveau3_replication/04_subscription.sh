e1ba35405c18fd40888ba7b91f09dbf7297d28fb
Mattis Tajan <mattis.tajan@ecoles-epsi.net>
2026-05-19 14:36:23 +0200

    Modification du script de subscription, Ajout des taches TODO du script

--- niveau3_replication/04_subscription.sh
+++ niveau3_replication/04_subscription.sh
@@ -1,27 +1,30 @@
-#!/bin/bash 
-# niveau3_replication/04_subscription.sh 
- 
-echo '=== [NIVEAU 3] Creation de la SUBSCRIPTION sur la cible ===' 
- 
-# Recuperer l'IP du container source 
-SOURCE_IP=$(docker inspect pg_montpellier \ 
-  --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}') echo "IP du container source : $SOURCE_IP" 
- 
-# Creer la subscription sur la cible 
-docker exec pg_toulouse psql -U admin -d techcorp_db -c \ 
-  "CREATE SUBSCRIPTION sub_techcorp 
-   CONNECTION 'host=$SOURCE_IP port=5432 dbname=techcorp_db user=admin password=admin123' 
-   PUBLICATION pub_techcorp;" 
- 
-echo 'OK : Subscription creee – replication en cours...' 
-sleep 5 
- 
-# Verifier l'etat de la replication 
-docker exec pg_toulouse psql -U admin -d techcorp_db -c \ 
-  "SELECT subname, subenabled FROM pg_subscription;" 
- 
-# Verifier que les donnees sont arrivees 
-docker exec pg_toulouse psql -U admin -d techcorp_db -c \   'SELECT COUNT(*) AS nb_utilisateurs FROM utilisateurs;' 
- 
-# TODO : Verifier l'etat detaille de la replication 
-# Indice : SELECT * FROM pg_stat_subscription; 
+#!/bin/bash
+# niveau3_replication/04_subscription.sh
+
+echo '=== [NIVEAU 3] Creation de la SUBSCRIPTION sur la cible ==='
+
+# Recuperer l'IP du container source
+SOURCE_IP=$(docker inspect pg_montpellier \
+  --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}')
+echo "IP du container source : $SOURCE_IP"
+
+# Creer la subscription sur la cible
+docker exec pg_toulouse psql -U admin -d techcorp_db -c \
+  "CREATE SUBSCRIPTION sub_techcorp
+  CONNECTION 'host=$SOURCE_IP port=5432 dbname=techcorp_db user=admin password=admin123'
+  PUBLICATION pub_techcorp;"
+
+echo 'OK : Subscription creee - replication en cours...'
+sleep 5
+
+# Verifier l'etat de la replication
+docker exec pg_toulouse psql -U admin -d techcorp_db -c \
+  "SELECT subname, subenabled FROM pg_subscription;"
+
+# Verifier que les donnees sont arrivees
+docker exec pg_toulouse psql -U admin -d techcorp_db -c \
+  'SELECT COUNT(*) AS nb_utilisateurs FROM utilisateurs;'
+
+# Verifier l'etat detaille de la replication
+docker exec pg_toulouse psql -U admin -d techcorp_db -c \
+  "SELECT * FROM pg_stat_subscription;"
