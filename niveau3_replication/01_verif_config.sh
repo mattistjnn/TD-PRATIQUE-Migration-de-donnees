3a63fbf55babd06c566cd031a7e1ad421acdf5dc
Mattis Tajan <mattis.tajan@ecoles-epsi.net>
2026-05-19 14:27:24 +0200

    Ajout du script de verifictation de config

---
+++ niveau3_replication/01_verif_config.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+# niveau3_replication/01_verif_config.sh 
+ 
+echo '=== [NIVEAU 3] Verification de la configuration ===' 
+ 
+WAL_LEVEL=$(docker exec pg_montpellier psql -U admin -t -c 'SHOW wal_level;' | tr -d 
+' ') 
+echo "WAL level source : $WAL_LEVEL" 
+ 
+if [ "$WAL_LEVEL" != 'logical' ]; then 
+    echo 'ERREUR : wal_level doit etre logical pour la replication'     exit 1 fi 
+ 
+echo 'OK : Configuration WAL correcte' 
+docker exec pg_montpellier psql -U admin -c 'SELECT version();' 
