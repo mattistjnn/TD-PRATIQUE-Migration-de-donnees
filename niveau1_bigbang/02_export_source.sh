ec9388cdb5a3cd0f4bb78a7bac9e1ac5a7eaac4b
Mattis Tajan <mattis.tajan@ecoles-epsi.net>
2026-05-19 16:40:08 +0200

    Modification du script d'exportation de la base source avec ajout d'une verification de fichier non vierge

--- niveau1_bigbang/02_export_source.sh
+++ niveau1_bigbang/02_export_source.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
-# niveau1_bigbang/02_export_source.sh
+
 echo '=== [NIVEAU 1] Export de la base source ==='
+
 BACKUP_DIR='./backups'
 BACKUP_FILE="$BACKUP_DIR/techcorp_bigbang_$(date +%Y%m%d_%H%M%S).dump"
 mkdir -p $BACKUP_DIR
-# Export complet en format custom (compresse)
+
 docker exec pg_montpellier pg_dump \
--U admin -d techcorp_db \
--F c -v \
--f /tmp/techcorp_backup.dump
-# Copier le fichier hors du container
+  -U admin -d techcorp_db \
+  -F c -v \
+  -f /tmp/techcorp_backup.dump
+
 docker cp pg_montpellier:/tmp/techcorp_backup.dump $BACKUP_FILE
+
 echo "OK : Sauvegarde creee : $BACKUP_FILE"
 ls -lh $BACKUP_FILE
+
+[ -s $BACKUP_FILE ] && echo 'OK : Fichier non vide' || echo 'ERREUR : fichier vide !'
